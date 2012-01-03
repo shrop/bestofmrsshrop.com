5d96bfde83ad2476f12e91f66ae1d3f9e38da762
Mark Shropshire <mdshrops@shropnet.net>
2012-01-02 21:51:23 -0500

    Updated Feeds module to 7.x-2.x-dev so that I can tag titles as unique and nodes are duplicated on each import. Issue fixed here: http://drupal.org/node/1206042.

--- www/sites/all/modules/feeds/feeds.api.php
+++ www/sites/all/modules/feeds/feeds.api.php
@@ -98,6 +98,21 @@ function hook_feeds_after_parse(FeedsSource $source, FeedsParserResult $result) 
 }
 
 /**
+ * Invoked before a feed item is saved.
+ *
+ * @param $source
+ *  FeedsSource object that describes the source that is being imported.
+ * @param $entity
+ *   The entity object.
+ */
+function hook_feeds_presave(FeedsSource $source, $entity) {
+  if ($entity->feeds_item->entity_type == 'node') {
+    // Skip saving this entity.
+    $entity->feeds_item->skip = TRUE;
+  }
+}
+
+/**
  * Invoked after a feed source has been imported.
  *
  * @param $source
